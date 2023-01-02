5b8b7135fca538afab185b358cd2eb6f3b496f59
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-01-02 13:39:58 -0500

    refactor(projects.jsx): removed console.log

--- components/work/Projects.jsx
+++ components/work/Projects.jsx
@@ -8,11 +8,9 @@ import theme from '../../theme/theme';
 import useBreakpoints from '@carpenjk/prop-x/useBreakpoints';
 
 const getProjects = (filter) => {
-  console.log("🚀 ~ file: Projects.jsx ~ line 11 ~ getProjects ~ filter", filter);
   const filtered = filter === 'all'
   ? projects
   : projects.filter((project) => project.type === filter);
-  console.log("🚀 ~ file: Projects.jsx ~ line 13 ~ getProjects ~ filtered", filtered);
   return filtered;
 };
       
